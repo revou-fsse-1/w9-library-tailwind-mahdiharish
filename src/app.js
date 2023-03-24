7f9565de51cd5162e1a15342962352a1490423e6
mahdiharish <mahdiharish12@gmail.com>
2023-03-25 05:47:54 +0800

    remove one function

--- src/app.js
+++ src/app.js
@@ -14,12 +14,6 @@ function toggleMobileMenu() {
         menu.classList.replace('block', 'hidden')
     }
 }
-function getPageFromUrl() {
-    const queryString = window.location.search
-    const urlParams = new URLSearchParams(queryString)
-    let page = urlParams.get('page')
-    return page
-}
 async function getBooksData() {
     let response = await fetch('./data.json')
     let json = await response.json()
