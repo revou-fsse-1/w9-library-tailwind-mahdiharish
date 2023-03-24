3133385b825b630c8d0eeb0e92d34ec5e68931c0
mahdiharish <mahdiharish12@gmail.com>
2023-03-25 00:27:52 +0800

    add docker and fix paths

--- src/app.js
+++ src/app.js
@@ -1,22 +1,39 @@
+function toggleMobileMenu() {
+    let menuButton = document.getElementById('m-menu-button')
+    let menu = document.getElementById('m-menu')
+
+    let menuIcon = menuButton.children[1]
+    let closeIcon = menuButton.children[2]
+
+    if (menuIcon.classList.contains('block')) {
+        menuIcon.classList.replace('block', 'hidden')
+        closeIcon.classList.replace('hidden', 'block')
+        menu.classList.replace('hidden', 'block')
+    } else {
+        menuIcon.classList.replace('hidden', 'block')
+        closeIcon.classList.replace('block', 'hidden')
+        menu.classList.replace('block', 'hidden')
+    }
+}
 function getPageFromUrl() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
 
-    let page = urlParams.get('page');
+    let page = urlParams.get('page')
 
-    return page;
+    return page
 }
 
 async function getBooksData() {
-    let response = await fetch('./data.json');
-    let json = await response.json();
-    return json['books'];
+    let response = await fetch('./data.json')
+    let json = await response.json()
+    return json['books']
 }
 
 /**
  * To get books data, you should make sure that your function is async function
  * and call `getBooksData` function with await.
- * 
+ *
  * e.g.
  * async function yourFunction() {
  *      let books = await getBooksData();
